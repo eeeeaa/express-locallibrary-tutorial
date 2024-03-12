efccd54669174dfffc9954b8e6d015aff66d5258
tiger <44923556+eeeeaa@users.noreply.github.com>
2024-03-12 20:40:18 +0700

    implement home page and home page controller as well as setting up controllers for data

---
+++ controllers/authorController.js
@@ -0,0 +1,55 @@
+const Author = require("../models/author");
+const asyncHandler = require("express-async-handler");
+/*
+Note: 
+asyncHandler is a 3rd-party wrapper module 
+that provide a wrapper function for using try-catch, something like this:
+
+async function (req, res, next) {
+  try {
+    const successfulResult = await About.find({}).exec();
+    res.render("about_view", { title: "About", list: successfulResult });
+  } catch (error) {
+    return next(error);
+  }
+}*/
+
+// Display list of all Authors.
+exports.author_list = asyncHandler(async (req, res, next) => {
+  res.send("NOT IMPLEMENTED: Author list");
+});
+
+// Display detail page for a specific Author.
+exports.author_detail = asyncHandler(async (req, res, next) => {
+  res.send(`NOT IMPLEMENTED: Author detail: ${req.params.id}`);
+});
+
+// Display Author create form on GET.
+exports.author_create_get = asyncHandler(async (req, res, next) => {
+  res.send("NOT IMPLEMENTED: Author create GET");
+});
+
+// Handle Author create on POST.
+exports.author_create_post = asyncHandler(async (req, res, next) => {
+  res.send("NOT IMPLEMENTED: Author create POST");
+});
+
+// Display Author delete form on GET.
+exports.author_delete_get = asyncHandler(async (req, res, next) => {
+  res.send("NOT IMPLEMENTED: Author delete GET");
+});
+
+// Handle Author delete on POST.
+exports.author_delete_post = asyncHandler(async (req, res, next) => {
+  res.send("NOT IMPLEMENTED: Author delete POST");
+});
+
+// Display Author update form on GET.
+exports.author_update_get = asyncHandler(async (req, res, next) => {
+  res.send("NOT IMPLEMENTED: Author update GET");
+});
+
+// Handle Author update on POST.
+exports.author_update_post = asyncHandler(async (req, res, next) => {
+  res.send("NOT IMPLEMENTED: Author update POST");
+});
