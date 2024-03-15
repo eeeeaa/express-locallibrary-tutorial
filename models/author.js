0b5560160bc3c49e7c8e8db6871f0fca8a57af33
tiger <44923556+eeeeaa@users.noreply.github.com>
2024-03-15 19:59:33 +0700

    implement delete and update for author data

--- models/author.js
+++ models/author.js
@@ -40,6 +40,14 @@ AuthorSchema.virtual("date_of_death_formatted").get(function () {
     : "unknown";
 });
 
+AuthorSchema.virtual("date_of_birth_yyyy_mm_dd").get(function () {
+  return DateTime.fromJSDate(this.date_of_birth).toISODate();
+});
+
+AuthorSchema.virtual("date_of_death_yyyy_mm_dd").get(function () {
+  return DateTime.fromJSDate(this.date_of_death).toISODate();
+});
+
 AuthorSchema.virtual("lifespan").get(function () {
   const birth = this.date_of_birth
     ? DateTime.fromJSDate(this.date_of_birth).toLocaleString(DateTime.DATE_MED)
