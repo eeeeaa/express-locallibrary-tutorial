e0562c56058dc01df11fcc0677e73ff254118b6d
tiger <44923556+eeeeaa@users.noreply.github.com>
2024-03-16 18:56:14 +0700

    install securities dependencies for deployment

--- app.js
+++ app.js
@@ -8,7 +8,19 @@ const indexRouter = require("./routes/index");
 const usersRouter = require("./routes/users");
 const catalogRouter = require("./routes/catalog");
 
+const compression = require("compression");
+const helmet = require("helmet");
+
 const app = express();
+
+// Set up rate limiter: maximum of twenty requests per minute
+const RateLimit = require("express-rate-limit");
+const limiter = RateLimit({
+  windowMs: 1 * 60 * 1000, // 1 minute
+  max: 20,
+});
+// Apply rate limiter to all requests
+app.use(limiter);
 
 const mongoose = require("mongoose");
 require("dotenv").config();
@@ -29,7 +41,20 @@ app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+//use compression to reduce time to transfer data between client/server
+app.use(compression());
+
 app.use(express.static(path.join(__dirname, "public")));
+
+//use helmet to protect against well-known web vulnerabilities
+app.use(
+  helmet.contentSecurityPolicy({
+    directives: {
+      "script-src": ["'self'", "code.jquery.com", "cdn.jsdelivr.net"],
+    },
+  })
+);
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
